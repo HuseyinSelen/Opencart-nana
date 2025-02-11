d8d7e1a4e6c17acdaa151c2f7088aa931c435931
HuseyinSelen <selen.huseyin98@gmail.com>
2025-02-11 10:43:15 +0300

    opencart nana project

---
+++ admin/language/tr-tr/sale/recurring.php
@@ -0,0 +1,82 @@
+<?php
+// Heading
+$_['heading_title']                        = 'Tekrarlanan Siparişler';
+
+// Text
+$_['text_success']                         = 'Başarılı: Tekrarlanan siparişleri değiştirdiniz!';
+$_['text_list']                            = 'Tekrarlanan Sipariş Listesi';
+$_['text_filter']                          = 'Filtre';
+$_['text_recurring_detail']                = 'Tekrarlanan Ayrıntıları';
+$_['text_order_detail']                    = 'Sipariş Detayları';
+$_['text_product_detail']                  = 'Ürün Detayları';
+$_['text_transaction']                     = 'İşlemler';
+$_['text_order_recurring_id']              = 'Tekrarlanan Sipariş ID';
+$_['text_reference']                       = 'Ödeme Referansı';
+$_['text_recurring_name']                  = 'İşlem Profil';
+$_['text_recurring_description']           = 'Açıklama';
+$_['text_recurring_status']                = 'Durum';
+$_['text_payment_method']                  = 'Ödeme Yöntemi';
+$_['text_order_id']                        = 'Sipariş ID';
+$_['text_customer']                        = 'Müşteri';
+$_['text_email']                           = 'Email';
+$_['text_date_added']                      = 'Tarih Eklendi';
+$_['text_order_status']                    = 'Sipariş Durum';
+$_['text_type']                            = 'Tip';
+$_['text_action']                          = 'İşlem';
+$_['text_product']                         = 'Ürün';
+$_['text_quantity']                        = 'Miktar';
+$_['text_amount']                          = 'Tutar';
+$_['text_cancel_payment']                  = 'Ödemeyi İptal Et';
+$_['text_status_1']                        = 'Aktif';
+$_['text_status_2']                        = 'Pasif';
+$_['text_status_3']                        = 'İptal Edildi';
+$_['text_status_4']                        = 'Askıya Alındı';
+$_['text_status_5']                        = 'Süresi Dolmuş';
+$_['text_status_6']                        = 'Beklemede';
+
+$_['text_transactions']                    = 'İşlemler';
+$_['text_cancel_confirm']                  = 'Profilin iptali geri alınamaz! Bunu yapmak istediğine emin misin?';
+$_['text_transaction_date_added']          = 'Tarih eklendi';
+$_['text_transaction_payment']             = 'Ödeme';
+$_['text_transaction_outstanding_payment'] = 'Ödenmemiş ödeme';
+$_['text_transaction_skipped']             = 'Ödeme atlandı';
+$_['text_transaction_failed']              = 'Ödeme başarısız';
+$_['text_transaction_cancelled']           = 'İptal edildi';
+$_['text_transaction_suspended']           = 'Askıya alındı';
+$_['text_transaction_suspended_failed']    = 'Başarısız ödemeden askıya alındı';
+$_['text_transaction_outstanding_failed']  = 'Ödenmemiş, ödeme başarısız oldu';
+$_['text_transaction_expired']             = 'Süresi dolmuş';
+$_['text_cancelled']                       = 'Yinelenen ödeme iptal edildi';
+
+// Column
+$_['column_order_recurring_id']             = 'İşlem ID';
+$_['column_order_id']                       = 'Sipariş ID';
+$_['column_reference']                      = 'Ödeme Referansı';
+$_['column_customer']                       = 'Müşteri';
+$_['column_date_added']                     = 'Ekleme Tarihi';
+$_['column_status']                         = 'Durum';
+$_['column_amount']                         = 'Tutar';
+$_['column_type']                           = 'Tip';
+$_['column_action']                         = 'İşlem';
+
+// Entry
+$_['entry_order_recurring_id']             = 'İşlem ID';
+$_['entry_order_id']                       = 'Sipariş ID';
+$_['entry_reference']                      = 'Ödeme Referansı';
+$_['entry_customer']                       = 'Müşteri';
+$_['entry_date_added']                     = 'Ekleme Tarihi';
+$_['entry_status']                         = 'Durum';
+$_['entry_type']                           = 'Tip';
+$_['entry_action']                         = 'İşlem';
+$_['entry_email']                          = 'Email';
+$_['entry_description']                    = 'İşlem Profili\ Bilgisi';
+$_['entry_product']                        = 'Ürün';
+$_['entry_quantity']                       = 'Adet';
+$_['entry_amount']                         = 'Tutar';
+$_['entry_recurring']                      = 'İşlem Profili';
+$_['entry_payment_method']                 = 'Ödeme Metodu';
+$_['entry_cancel_payment']                 = 'Ödemeyi İptal Et';
+
+// Error
+$_['error_not_cancelled']                  = 'Hata: %s';
+$_['error_not_found']                      = 'Tekrarlanan işlem profili iptal edilemedi';
---
+++ catalog/language/tr-tr/common/footer.php
@@ -0,0 +1,19 @@
+<?php
+/* Turkceye Ceviren Safir Medya - https://www.safirmedya.com */
+
+// Text
+$_['text_information']  = 'Bilgiler';
+$_['text_service']      = 'Müşteri Servisi';
+$_['text_extra']        = 'Ekstralar';
+$_['text_contact']      = 'İletişim';
+$_['text_return']       = 'Ürün İadesi';
+$_['text_sitemap']      = 'Site Haritası';
+$_['text_manufacturer'] = 'Markalar';
+$_['text_voucher']      = 'Hediye Çeki';
+$_['text_affiliate']    = 'Ortaklık Programı';
+$_['text_special']      = 'Kampanyalar';
+$_['text_account']      = 'Hesabım';
+$_['text_order']        = 'Siparişlerim';
+$_['text_wishlist']     = 'Alışveriş Listem';
+$_['text_newsletter']   = 'Bülten Aboneliği';
+$_['text_powered']      = '<a href="https://www.safirmedya.com" target="_blank" title="Safir Medya E-Ticaret - Organik Trafik"><img src="https://www.safirmedya.com/uploads/images/logo-icon.png" alt="Safir Medya"></a> %s &copy; %s - Tüm Hakları Saklıdır.  ';
